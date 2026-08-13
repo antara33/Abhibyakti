aa5e73a0b94af579a06df2623fa103f359bb272b
antara33 <antaram330@gmail.com>
2026-08-14 02:29:51 +0600

    successfully implemented and tested lexer for Bangla compiler

---
+++ src/lexer.h
@@ -0,0 +1,29 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include "token.h"
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Lexer {
+private:
+    std::string source;
+    std::vector<Token> tokens;
+
+    size_t start;
+    size_t current;
+    int line;
+
+  std::unordered_map<std::string, BhashaTokenType> keywords;
+    void scanToken();
+    void number();
+    void identifier();
+
+public:
+    Lexer(const std::string& source);
+
+    std::vector<Token> tokenize();
+};
+
+#endif
---
+++ src/token.h
@@ -0,0 +1,94 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <string>
+
+// Avoid Windows macro conflicts
+#ifdef TRUE
+#undef TRUE
+#endif
+
+#ifdef FALSE
+#undef FALSE
+#endif
+
+enum class BhashaTokenType {
+
+    // Data Types
+    NUMBER_TYPE,
+    DECIMAL_TYPE,
+    STRING_TYPE,
+    CHAR_TYPE,
+    BOOLEAN_TYPE,
+    LIST_TYPE,
+    LONG_TYPE,
+    POSITIVE_TYPE,
+    VOID_TYPE,
+
+    // Keywords
+    IF,
+    ELSE,
+    WHILE,
+    PRINT,
+
+    // Boolean Values
+    BOOLEAN_TRUE,
+    BOOLEAN_FALSE,
+
+    // Identifiers and Literals
+    IDENTIFIER,
+    INTEGER,
+    DECIMAL,
+    STRING,
+    CHARACTER,
+
+    // Arithmetic Operators
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    MODULO,
+
+    // Relational Operators
+    GREATER,
+    LESS,
+    GREATER_EQUAL,
+    LESS_EQUAL,
+    EQUAL_EQUAL,
+    NOT_EQUAL,
+
+    // Logical Operators
+    AND,
+    OR,
+    NOT,
+
+    // Assignment
+    ASSIGN,
+
+    // Symbols
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+
+    SEMICOLON,
+    COMMA,
+
+    // Special
+    END_OF_FILE,
+    UNKNOWN
+};
+
+struct Token {
+
+    BhashaTokenType type;
+    std::string lexeme;
+    int line;
+
+    Token(BhashaTokenType type, std::string lexeme, int line)
+        : type(type), lexeme(lexeme), line(line) {}
+};
+
+#endif
